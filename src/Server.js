c2107f7c4a36b4398a862f904eb36bf1efed0dec
Caroline Tippins <carotippins@hotmail.com>
2022-07-17 19:33:03 -0400

    Fixed post and delete bugs

--- src/Server.js
+++ src/Server.js
@@ -1,25 +1,24 @@
 import { createServer, Model } from "miragejs"
 
-let blogs = [
-  { id: 1, title: "Yoga and Injuries", body: "Some yoga poses may lead to injuries if not implemented properly. There have been reports of knee and back injuries due to badly performed poses such as the pigeon pose leading to knee pain.", author: "John" },
-  // { id: 2, title: "My Second Blog", body: "Yoga is the best medicine for any anxiety issue. Doctors are urging their patient to try out this form of excercise as it has proven benefits.", author: "Nancy" },
-]
+// let blogs = [
+//   { id: 1, title: "Yoga and Injuries", body: "Some yoga poses may lead to injuries if not implemented properly. There have been reports of knee and back injuries due to badly performed poses such as the pigeon pose leading to knee pain.", author: "John" },
+//   // { id: 2, title: "My Second Blog", body: "Yoga is the best medicine for any anxiety issue. Doctors are urging their patient to try out this form of excercise as it has proven benefits.", author: "Nancy" },
+// ]
 createServer({
   models: {
-    blogs: Model,
+    blog: Model,
   },
-
   seeds(server) {
-    server.schema.blogs.create({ title: "Yoga and Injuries", body: "Some yoga poses may lead to injuries if not implemented properly. There have been reports of knee and back injuries due to badly performed poses such as the pigeon pose leading to knee pain.", author: "John" })
+    server.create('blog', {
+      title: "Yoga and Injuries",
+      body: "Some yoga poses may lead to injuries if not implemented properly. There have been reports of knee and back injuries due to badly performed poses such as the pigeon pose leading to knee pain.",
+      author: "John" ,
+    })
   },
   routes() {
     this.namespace = "api"
 
-    this.get("/blogs", () => {
-      return {
-        blogs
-      }
-    })
+    this.get('/blogs')
 
     // this.patch("/blogs/:id")
     this.patch('/blogs/:id', (schema, request) => {
@@ -35,11 +34,7 @@ createServer({
     // Responding to a POST request
     this.post("/blogs", (schema, request) => {
       let attrs = JSON.parse(request.requestBody)
-      attrs.id = Math.floor(Math.random() * 100)
-
-      blogs.push(attrs)
-
-      return { blog: attrs }
+      return schema.blogs.create(attrs)
     })
   },
 })
